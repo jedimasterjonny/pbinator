9f83851c9e089262aa22cc157aec881ce20b6100
jedimasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2026-05-02 20:37:38 +0100

    feat: add SQLModel table models

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,5 +1,12 @@
 import pbinator
+from pbinator import models
 
 
 def test_package_imports() -> None:
     assert pbinator is not None
+
+
+def test_models_imports() -> None:
+    assert models.Activity.__tablename__ == "activity"
+    assert models.BestEffort.__tablename__ == "best_effort"
+    assert models.SyncCursor.__tablename__ == "sync_cursor"
